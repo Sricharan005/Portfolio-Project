7bb653c7f2a8b289e79519c354f41b6e98467be6
Sricharan Gandham <162398825+Sricharan005@users.noreply.github.com>
2024-06-09 16:47:30 +0530

    Create App.js
    from perplexity

---
+++ App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Timeline from './Timeline';
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+      <Timeline />
+    </div>
+  );
+};
+
+export default App;
