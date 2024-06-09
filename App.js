bebe2438dc1d9ce9dc0044206e66bbf098d9eeec
Sricharan Gandham <162398825+Sricharan005@users.noreply.github.com>
2024-06-09 18:19:57 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,27 +5,27 @@ import Projects from './Projects';
 import Timeline from './Timeline';
 
 const App = () => {
-    return (
-        <div>
-            <Hero />
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#timeline">Timeline</a>
-                    </li>
-                </ul>
-            </nav>
-            <About />
-            <Projects />
-            <Timeline />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Hero />
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#timeline">Timeline</a>
+          </li>
+        </ul>
+      </nav>
+      <About />
+      <Projects />
+      <Timeline />
+    </div>
+  );
 };
 
 export default App;
